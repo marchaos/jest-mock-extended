e97c0a7f9abc321096f8a3ba0e780c367cd3a752
marchaos <marchaos@gmail.com>
2019-11-26 08:11:22 +0000

    ignore lib dir.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ["/node_modules/", '/lib/'],
   coveragePathIgnorePatterns: ["/node_modules/", '/lib/']
 };
