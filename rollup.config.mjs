a003508dcc26f81b0ebcb77b26f99868a366b37d
Eike Lurz <eike.lurz@sportalliance.com>
2022-11-01 20:38:08 +0100

    fix: roll back packaging to pure esm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,27 +2,21 @@ import pkg from './package.json' assert { type: 'json' };
 import esbuild from 'rollup-plugin-esbuild';
 import { defineConfig } from 'rollup';
 
-const config = defineConfig([
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: 'lib/cjs/index.js',
-                format: 'cjs',
-            },
-            {
-                file: 'lib/esm/index.mjs',
-                format: 'es',
-            },
-        ],
-        external: Object.keys(pkg.dependencies),
-        plugins: [
-            esbuild({
-                target: 'node18',
-                tsconfig: 'tsconfig.json',
-            }),
-        ],
-    },
-]);
+const config = defineConfig({
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'lib/index.js',
+            format: 'es',
+        },
+    ],
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+        esbuild({
+            target: 'node18',
+            tsconfig: 'tsconfig.json',
+        }),
+    ],
+});
 
 export default config;
