02eb750a82f89588b6d93dc4faaf55d3d5eba479
skovhus <kenneth.skovhus@gmail.com>
2023-02-27 10:59:30 +0100

    Correct pass fallbackMockImplementation

--- src/CalledWithFn.ts
+++ src/CalledWithFn.ts
@@ -13,8 +13,12 @@ function isJestAsymmetricMatcher(obj: any): obj is JestAsymmetricMatcher {
     return !!obj && typeof obj === 'object' && 'asymmetricMatch' in obj && typeof obj.asymmetricMatch === 'function';
 }
 
-const checkCalledWith = <T, Y extends any[]>(calledWithStack: CalledWithStackItem<T, Y>[], actualArgs: Y): T => {
-    const calledWithInstance = calledWithStack.find(instance =>
+const checkCalledWith = <T, Y extends any[]>(
+    calledWithStack: CalledWithStackItem<T, Y>[],
+    actualArgs: Y,
+    fallbackMockImplementation?: (...args: Y) => T
+): T => {
+    const calledWithInstance = calledWithStack.find((instance) =>
         instance.args.every((matcher, i) => {
             if (matcher instanceof Matcher) {
                 return matcher.asymmetricMatch(actualArgs[i]);
@@ -29,7 +33,9 @@ const checkCalledWith = <T, Y extends any[]>(calledWithStack: CalledWithStackIte
     );
 
     // @ts-ignore cannot return undefined, but this will fail the test if there is an expectation which is what we want
-    return calledWithInstance ? calledWithInstance.calledWithFn(...actualArgs) : undefined;
+    return calledWithInstance
+        ? calledWithInstance.calledWithFn(...actualArgs)
+        : fallbackMockImplementation && fallbackMockImplementation(...actualArgs);
 };
 
 export const calledWithFn = <T, Y extends any[]>({
@@ -42,9 +48,10 @@ export const calledWithFn = <T, Y extends any[]>({
         // We create new function to delegate any interactions (mockReturnValue etc.) to for this set of args.
         // If that set of args is matched, we just call that jest.fn() for the result.
         const calledWithFn = jest.fn(fallbackMockImplementation);
-        if (!fn.getMockImplementation()) {
+        const mockImplementation = fn.getMockImplementation();
+        if (!mockImplementation || mockImplementation === fallbackMockImplementation) {
             // Our original function gets a mock implementation which handles the matching
-            fn.mockImplementation((...args: Y) => checkCalledWith(calledWithStack, args));
+            fn.mockImplementation((...args: Y) => checkCalledWith(calledWithStack, args, fallbackMockImplementation));
             calledWithStack = [];
         }
         calledWithStack.unshift({ args, calledWithFn });
