c432fe97b4d720355ffa952bf1e5ec0f3c789eba
Eike Lurz <eike.lurz@tuta.io>
2023-09-29 15:21:57 +0200

    🔧 Remove trainling semicolonos

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,15 +1,15 @@
-import { mockDeep, DeepMockProxy } from 'vitest-mock-extended';
+import { mockDeep, DeepMockProxy } from 'vitest-mock-extended'
 
 type thing = {
-    property: string;
-};
+    property: string
+}
 
 export type MockContext = {
-    prisma: DeepMockProxy<thing>;
-};
+    prisma: DeepMockProxy<thing>
+}
 
 export const createMockContext = (): MockContext => {
     return {
         prisma: mockDeep<thing>(),
-    };
-};
+    }
+}
--- src/Matchers.ts
+++ src/Matchers.ts
@@ -1,102 +1,102 @@
-type MatcherFn<T> = (actualValue: T) => boolean;
+type MatcherFn<T> = (actualValue: T) => boolean
 
 // needs to be a class so we can instanceof
 class Matcher<T> {
-    $$typeof: symbol;
-    inverse?: boolean;
+    $$typeof: symbol
+    inverse?: boolean
 
     constructor(
         readonly asymmetricMatch: MatcherFn<T>,
         private readonly description: string,
     ) {
-        this.$$typeof = Symbol.for('vi.asymmetricMatcher');
+        this.$$typeof = Symbol.for('vi.asymmetricMatcher')
     }
 
     toString() {
-        return this.description;
+        return this.description
     }
 
     toAsymmetricMatcher() {
-        return this.description;
+        return this.description
     }
 
     getExpectedType() {
-        return 'undefined';
+        return 'undefined'
     }
 }
 
 class CaptorMatcher<T> {
-    $$typeof: symbol;
-    public readonly asymmetricMatch: MatcherFn<T>;
-    public readonly value!: T;
-    public readonly values: T[] = [];
+    $$typeof: symbol
+    public readonly asymmetricMatch: MatcherFn<T>
+    public readonly value!: T
+    public readonly values: T[] = []
     constructor() {
-        this.$$typeof = Symbol.for('vi.asymmetricMatcher');
+        this.$$typeof = Symbol.for('vi.asymmetricMatcher')
 
         this.asymmetricMatch = (actualValue: T) => {
             // @ts-ignore
-            this.value = actualValue;
-            this.values.push(actualValue);
-            return true;
-        };
+            this.value = actualValue
+            this.values.push(actualValue)
+            return true
+        }
     }
 
     getExpectedType() {
-        return 'Object';
+        return 'Object'
     }
 
     toString() {
-        return 'captor';
+        return 'captor'
     }
 
     toAsymmetricMatcher() {
-        return 'captor';
+        return 'captor'
     }
 }
 
 interface MatcherCreator<T, E = T> {
-    (expectedValue?: E): Matcher<T>;
+    (expectedValue?: E): Matcher<T>
 }
 
-type MatchersOrLiterals<Y extends any[]> = { [K in keyof Y]: Matcher<Y[K]> | Y[K] };
+type MatchersOrLiterals<Y extends any[]> = { [K in keyof Y]: Matcher<Y[K]> | Y[K] }
 
-const any: MatcherCreator<any> = () => new Matcher(() => true, 'any()');
-const anyBoolean: MatcherCreator<boolean> = () => new Matcher((actualValue: boolean) => typeof actualValue === 'boolean', 'anyBoolean()');
+const any: MatcherCreator<any> = () => new Matcher(() => true, 'any()')
+const anyBoolean: MatcherCreator<boolean> = () => new Matcher((actualValue: boolean) => typeof actualValue === 'boolean', 'anyBoolean()')
 const anyNumber: MatcherCreator<number> = () =>
-    new Matcher((actualValue) => typeof actualValue === 'number' && !isNaN(actualValue), 'anyNumber()');
-const anyString: MatcherCreator<string> = () => new Matcher((actualValue: string) => typeof actualValue === 'string', 'anyString()');
+    new Matcher((actualValue) => typeof actualValue === 'number' && !isNaN(actualValue), 'anyNumber()')
+const anyString: MatcherCreator<string> = () => new Matcher((actualValue: string) => typeof actualValue === 'string', 'anyString()')
 const anyFunction: MatcherCreator<Function> = () =>
-    new Matcher((actualValue: Function) => typeof actualValue === 'function', 'anyFunction()');
-const anySymbol: MatcherCreator<symbol> = () => new Matcher((actualValue) => typeof actualValue === 'symbol', 'anySymbol()');
+    new Matcher((actualValue: Function) => typeof actualValue === 'function', 'anyFunction()')
+const anySymbol: MatcherCreator<symbol> = () => new Matcher((actualValue) => typeof actualValue === 'symbol', 'anySymbol()')
 const anyObject: MatcherCreator<any> = () =>
-    new Matcher((actualValue) => typeof actualValue === 'object' && actualValue !== null, 'anyObject()');
+    new Matcher((actualValue) => typeof actualValue === 'object' && actualValue !== null, 'anyObject()')
 
-const anyArray: MatcherCreator<any[]> = () => new Matcher((actualValue) => Array.isArray(actualValue), 'anyArray()');
-const anyMap: MatcherCreator<Map<any, any>> = () => new Matcher((actualValue) => actualValue instanceof Map, 'anyMap()');
-const anySet: MatcherCreator<Set<any>> = () => new Matcher((actualValue) => actualValue instanceof Set, 'anySet()');
-const isA: MatcherCreator<any> = (clazz) => new Matcher((actualValue) => actualValue instanceof clazz, 'isA()');
+const anyArray: MatcherCreator<any[]> = () => new Matcher((actualValue) => Array.isArray(actualValue), 'anyArray()')
+const anyMap: MatcherCreator<Map<any, any>> = () => new Matcher((actualValue) => actualValue instanceof Map, 'anyMap()')
+const anySet: MatcherCreator<Set<any>> = () => new Matcher((actualValue) => actualValue instanceof Set, 'anySet()')
+const isA: MatcherCreator<any> = (clazz) => new Matcher((actualValue) => actualValue instanceof clazz, 'isA()')
 
 const arrayIncludes: MatcherCreator<any[], any> = (arrayVal) =>
-    new Matcher((actualValue) => Array.isArray(actualValue) && actualValue.includes(arrayVal), 'arrayIncludes()');
+    new Matcher((actualValue) => Array.isArray(actualValue) && actualValue.includes(arrayVal), 'arrayIncludes()')
 const setHas: MatcherCreator<Set<any>, any> = (arrayVal) =>
-    new Matcher((actualValue) => anySet().asymmetricMatch(actualValue) && actualValue!.has(arrayVal), 'setHas()');
+    new Matcher((actualValue) => anySet().asymmetricMatch(actualValue) && actualValue!.has(arrayVal), 'setHas()')
 const mapHas: MatcherCreator<Map<any, any>, any> = (mapVal) =>
-    new Matcher((actualValue) => anyMap().asymmetricMatch(actualValue) && actualValue!.has(mapVal), 'mapHas()');
+    new Matcher((actualValue) => anyMap().asymmetricMatch(actualValue) && actualValue!.has(mapVal), 'mapHas()')
 const objectContainsKey: MatcherCreator<any, string> = (key) =>
-    new Matcher((actualValue) => anyObject().asymmetricMatch(actualValue) && actualValue[key!] !== undefined, 'objectContainsKey()');
+    new Matcher((actualValue) => anyObject().asymmetricMatch(actualValue) && actualValue[key!] !== undefined, 'objectContainsKey()')
 const objectContainsValue: MatcherCreator<any> = (value) =>
     new Matcher(
         (actualValue) => anyObject().asymmetricMatch(actualValue) && Object.values(actualValue).includes(value),
         'objectContainsValue()',
-    );
+    )
 
-const notNull: MatcherCreator<any> = () => new Matcher((actualValue) => actualValue !== null, 'notNull()');
-const notUndefined: MatcherCreator<any> = () => new Matcher((actualValue) => actualValue !== undefined, 'notUndefined()');
+const notNull: MatcherCreator<any> = () => new Matcher((actualValue) => actualValue !== null, 'notNull()')
+const notUndefined: MatcherCreator<any> = () => new Matcher((actualValue) => actualValue !== undefined, 'notUndefined()')
 const notEmpty: MatcherCreator<any> = () =>
-    new Matcher((actualValue) => actualValue !== null && actualValue !== undefined && actualValue !== '', 'notEmpty()');
+    new Matcher((actualValue) => actualValue !== null && actualValue !== undefined && actualValue !== '', 'notEmpty()')
 
-const captor = <T extends any = any>() => new CaptorMatcher<T>();
-const matches = <T extends any = any>(matcher: MatcherFn<T>) => new Matcher(matcher, 'matches()');
+const captor = <T extends any = any>() => new CaptorMatcher<T>()
+const matches = <T extends any = any>(matcher: MatcherFn<T>) => new Matcher(matcher, 'matches()')
 
 export {
     Matcher,
@@ -122,5 +122,5 @@ export {
     notEmpty,
     captor,
     matches,
-};
-export type { MatcherFn, MatchersOrLiterals, MatcherCreator };
+}
+export type { MatcherFn, MatchersOrLiterals, MatcherCreator }
